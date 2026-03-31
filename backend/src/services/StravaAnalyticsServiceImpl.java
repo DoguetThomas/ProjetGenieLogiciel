9e892f624d6f7d6203849e40d400db11559f6b98
DoguetThomas <thomasdoguet22@icloud.com>
2026-03-31 10:51:52 +0200

    reset avec seulement le parser et StravaRecord

--- backend/src/services/StravaAnalyticsServiceImpl.java
+++ backend/src/services/StravaAnalyticsServiceImpl.java
@@ -8,62 +8,9 @@ import java.util.List;
 import java.util.Map;
 
 public class StravaAnalyticsServiceImpl implements AnalyticsService{
-    //liste des points qui seront récoltés sur le csv
-    private final List<StravaRecord> records;
-    //on fait tourner la méthode readCSV pour récupérer toute les lignes du CSV
-    public StravaAnalyticsServiceImpl(String filePath) {
-        this.records = StravaCsvReader.readCsv(filePath);
-    }
-
     @Override
-    public AllActivitiesDto getAllActivities() {
-        //on va déterminer les distances et les vitesses maximales de chaque activités
-        Map<String, Double> maxDistances = new HashMap<>();
-        Map<String, Double> maxSpeeds = new HashMap<>();
-        //on parcourt chaque points pour les déterminer
-        for (StravaRecord record : this.records) {
-            String activityId = record.getDatafile();
-            //on passe les activités indétéctables
-            if (activityId == null || activityId.trim().isEmpty()) {
-                continue;
-            }
-            Double distance = record.getDistance();
-            if (distance != null) {
-                //récupération de la distance précédente si existe (0 sinon)
-                double previousMax = maxDistances.getOrDefault(activityId, 0.0);
-                //comparaison et remplacement si besoin;
-                if (distance > previousMax) {
-                    maxDistances.put(activityId, distance);
-                }
-            }
-            Double speed = record.getEnhancedSpeed();
-            if (speed != null) {
-                //récupération de la vitesse max si existe (0 sinon)
-                double previousMaxSpeed = maxSpeeds.getOrDefault(activityId, 0.0);
-                //comparaison et remplacement si besoin;
-                if (speed > previousMaxSpeed) {
-                    maxSpeeds.put(activityId, speed);
-                }
-            }
-        }
-        // 2. Création du DTO de retour
-        AllActivitiesDto resultDto = new AllActivitiesDto(new ArrayList<>());
-        for (Map.Entry<String, Double> entry : maxDistances.entrySet()) {
-            String id = entry.getKey();
-            //récupération de la distance et conversion en Km et récupération de la vitesse
-            double distanceKm = entry.getValue() / 1000.0;
-            double maxSpeedForActivity = maxSpeeds.getOrDefault(id, 0.0);
-            //détermination du type de sport (j'ai fixé a 6 m/s mais discutable)
-            ActivityTypeDto sportType;
-            if (maxSpeedForActivity > 6) {
-                sportType = ActivityTypeDto.BIKE;
-            } else {
-                sportType = ActivityTypeDto.RUN;
-            }
-            //ajout dans le allActivitiesDto l'activité détéctée et analysée
-            resultDto.addActivity(id, sportType, distanceKm);
-        }
-        return resultDto;
+    public AllActivitiesDto getAllActivities(){
+        return null;
     }
 
     @Override
