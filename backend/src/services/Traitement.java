d80fcd0bf87b7fa791624f9fa505a72d458809ca
DoguetThomas <thomasdoguet22@icloud.com>
2026-04-01 14:47:25 +0200

    inititalisation du fichier Traitement avec chauqe méthode vide
    la logique du constructeur initialisée ( User dans le paramètres à discuter )

---
+++ backend/src/services/Traitement.java
@@ -0,0 +1,71 @@
+package services;
+
+import model.User;
+import model.Activity;
+import model.StravaRecord;
+
+import java.util.List;
+import java.util.Map;
+
+public class Traitement {
+    private List<services.StravaRecord> records;
+    private Map<String, List<StravaRecord>> sortedRecords;
+    private User user;
+
+    public Traitement(String filePath, User user){
+        this.records = StravaCsvReader.readCsv(filePath);
+        this.sortedRecords = this.RecordSorter();
+        this.user = user;
+    }
+
+    private Map<String, List<StravaRecord>> RecordSorter(){
+        return null;
+    }
+
+    private String determineSportType(String id){
+        return null;
+    }
+
+    private Double getDist(String id){
+        return null;
+    }
+
+    private Double getDuration(String id){
+        return null;
+    }
+
+    private Double getAvgSpeed(String id){
+        return null;
+    }
+
+    private Double getAvgPace(String id){
+        return null;
+    }
+
+    private Double getAvgHR(String id){
+        return null;
+    }
+
+    private Double getMaxHR(String id){
+        return null;
+    }
+
+    private Double getAvgPower(String id){
+        return null;
+    }
+
+    /*il faut qu'on détermine comment représenter le Split
+    public ??? getSplit(){
+        return null;
+    }
+     */
+
+    private Double[] calculateTimeInZones(){
+        return null;
+    }
+
+    public List<Activity> getActivities(){
+        return null;
+    }
+
+}
