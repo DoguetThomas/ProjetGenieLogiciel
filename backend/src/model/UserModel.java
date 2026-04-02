30d8c576a05b4b5642d51bc9d05b65f26778fc58
antoine_lpp <antoine.boulagnon@gmail.com>
2026-04-02 15:21:50 +0200

    Création de l'UserModel (interface) et transfer de User vers UserImpl

---
+++ backend/src/model/UserModel.java
@@ -0,0 +1,53 @@
+package model;
+
+public interface UserModel {
+
+    /**
+     * Récupère l'âge de l'utilisateur.
+     * @return L'âge de l'utilisateur en années.
+     */
+    public int getAge(int age);
+
+    /**
+     * Définit l'âge de l'utilisateur.
+     * @param age Le nouvel âge de l'utilisateur.
+     */
+    public void setAge(int age);
+
+    /**
+     * Récupère l'état de l'attribut booléen genre.
+     * @return La valeur booléenne de l'attribut genre.
+     */
+    public boolean getGenre(boolean genre);
+
+    /**
+     * Définit l'état de l'attribut booléen genre.
+     * @param genre La nouvelle valeur booléenne.
+     */
+    public void setGenre(boolean genre);
+
+    /**
+     * Récupère la taille de l'utilisateur.
+     * @return La taille de l'utilisateur.
+     */
+    public double getHeight();
+
+    /**
+     * Définit la taille de l'utilisateur.
+     * @param height La nouvelle taille de l'utilisateur.
+     */
+    public void setHeight(double height);
+
+    /**
+     * Récupère le poids de l'utilisateur.
+     * @return Le poids de l'utilisateur.
+     */
+    public double getWeight();
+
+    /**
+     * Définit le poids de l'utilisateur.
+     * @param weight Le nouveau poids de l'utilisateur.
+     */
+    public void setWeight(double weight);
+
+}
