1031385e1b56d9f30f87ec5f8fc7aaed71057382
natm1685 <nmallard85@gmail.com>
2026-04-08 16:14:25 +0200

    Création class test StravaUserProfileServiceImplTest

---
+++ backend/test/StravaUserProfileServiceImplTest.java
@@ -0,0 +1,12 @@
+import dto.UserProfileDto;
+import org.junit.jupiter.api.Test;
+
+public class StravaUserProfileServiceImplTest {
+
+    /*@Test
+    void(){
+        UserProfileDto userProfileDto = new UserProfileDto();
+
+    }*/
+
+}
